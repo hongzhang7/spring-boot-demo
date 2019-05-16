bb68fd2e9e94ee0d248f9c812124fe9e3dd0d322
动感酸辣粉儿 <1103031265@qq.com>
2019-05-16 17:08:15 +0800

    Create ProjectPermission.java

---
+++ src/main/java/com/app/web/annotations/ProjectPermission.java
@@ -0,0 +1,16 @@
+package com.app.web.annotations;
+
+import com.app.core.model.RoleEnum;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ProjectPermission {
+
+    RoleEnum value();
+}
+
